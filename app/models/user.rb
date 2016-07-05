95031f2c52074f7dea158b0baeca1f21fa600b96
Daniel <daniel.steenberg@gmail.com>
2016-07-05 21:58:58 +0200

    Convert erb to slim and few corrections

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,11 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    user = User.where(provider: access_token.provider, uid: access_token.uid ).first
     if user
       return user
     else
-      registered_user = User.where(:email => access_token.info.email).first
+      registered_user = User.where(email: access_token.info.email).first
       if registered_user
         return registered_user
       else
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
           provider:access_token.provider,
           email: data["email"],
           uid: access_token.uid ,
-          password: Devise.friendly_token[0,20],
+          password: Devise.friendly_token[0,20]
         )
       end
    end
